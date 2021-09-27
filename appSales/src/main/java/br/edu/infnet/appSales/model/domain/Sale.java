4896b41875203a11bcd0eb48df01fe9a8704a8a9
Alan Pereira de Sá <alkinsret@gmail.com>
2021-09-26 23:06:07 -0300

    update sales

--- appSales/src/main/java/br/edu/infnet/appSales/model/domain/Sale.java
+++ appSales/src/main/java/br/edu/infnet/appSales/model/domain/Sale.java
@@ -5,9 +5,61 @@
  */
 package br.edu.infnet.appSales.model.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 /**
  *
  * @author Alan Sá
  */
 public class Sale {
+  private String description;
+  private LocalDateTime date;
+  private Client client;
+  private Property property;
+  
+  
+  public Sale() {
+		data = LocalDateTime.now();
+	}
+	
+	@Override
+	public String toString() {
+		
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(description);
+		sb.append(";");
+		sb.append(date.format(format));
+		sb.append(";");
+		sb.append(client.toString());
+		sb.append(";");
+    sb.append(property.toString());
+		}
+		
+		return sb.toString();
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Property getProperty() {
+		return property;
+	}
+
+	public void setProdutos(Property property) {
+		this.property = property;
+	}
 }
